b7f8c0578d35550ffcc5d5998b11f3eb3964e1cb
Tran Van Loc <tranvanloccntt123@gmail.com>
2022-06-30 16:27:32 +0700

    Remove folder public in project

--- resources/views/components/admin/nav-bar.blade.php
+++ resources/views/components/admin/nav-bar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-admin-nav">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Tei / CMS</a>
+      <a class="navbar-brand" href="#">TEI / CMS</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-admin-header" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
