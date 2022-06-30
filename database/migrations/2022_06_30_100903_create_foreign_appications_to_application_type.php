87f4b06ca62380270d2ff69120e59f49f71e62d6
Tran Van Loc <tranvanloccntt123@gmail.com>
2022-06-30 17:26:19 +0700

    Update type_id for applications table

---
+++ database/migrations/2022_06_30_100903_create_foreign_appications_to_application_type.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            Schema::disableForeignKeyConstraints();
+            //
+            $table->unsignedBigInteger("type_id");
+            $table->foreign("type_id")->references("id")->on("application_type");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            //
+        });
+    }
+};
