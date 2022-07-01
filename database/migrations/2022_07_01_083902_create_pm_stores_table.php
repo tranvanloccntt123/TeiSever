4e16fb376d682e33df50a9dd2cdfa2a84c5b182b
Tran Van Loc <tranvanloccntt123@gmail.com>
2022-07-01 15:43:12 +0700

    Create pm_stores table

---
+++ database/migrations/2022_07_01_083902_create_pm_stores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pm_stores', function (Blueprint $table) {
+            $table->id();
+            $table->string("option1")->nullable();
+            $table->string("option2")->nullable();
+            $table->string("option3")->nullable();
+            $table->integer("nums")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pm_stores');
+    }
+};
